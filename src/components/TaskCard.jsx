d5f5edd01247d15f2d2eeb41344e8d651f85de1a
LuisOsorioE <luisfelipeosorioe@gmail.com>
2024-11-03 23:51:15 -0600

    Revert "Merge branch 'main' of https://github.com/lfjOE/To-do_Logos"
    This reverts commit 634e9b850f31aac03febc0b208e1d9df3a4851c3, reversing
    changes made to 72fe3bc571b1f96d5cc742aaccca60bce23e5b91.

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -13,7 +13,7 @@ export default function TaskCard({ task, removeTask, phrase }) {
     <div className={`bg-[#222020] p-4 rounded-lg shadow-lg mb-4 shadow-black flex justify-between items-center transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
       <span className="w-11/12">
       <h3 className="text-white overflow-wrap break-words w-full">{task}</h3>
-      <h2 className="text-gray-400 w-full mt-2">{phrase}</h2>
+      <h2 className="text-gray-400 w-full">Lorem Ipsum Dolor</h2>
       </span>
       
       <button
@@ -28,4 +28,3 @@ export default function TaskCard({ task, removeTask, phrase }) {
     </div>
   );
 }
-
