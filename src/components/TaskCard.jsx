a07741bbb494639be0c91182f539101273f0bfda
LuisOsorioE <luisfelipeosorioe@gmail.com>
2025-01-27 00:15:58 -0600

    ya ni se

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,19 +1,21 @@
 import { MdDelete } from "react-icons/md";
 import { useState } from "react";
 
-export default function TaskCard({ task, removeTask, phrase }) {
+export default function TaskCard({ task, removeTask, phrase, taskId }) {
   const [isVisible, setIsVisible] = useState(true);
 
   const handleRemove = () => {
     setIsVisible(false); // Cambia el estado a invisible
-    setTimeout(removeTask, 300); // Llama a la función removeTask después de 300 ms (la duración de la animación)
+    setTimeout(() => {
+      removeTask(taskId); // Llama a removeTask después de 300 ms (la duración de la animación)
+    }, 300); // Asegúrate de que este tiempo coincida con la duración de la animación
   };
 
   return (
     <div className={`bg-[#222020] p-4 rounded-lg shadow-lg mb-4 shadow-black flex justify-between items-center transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
       <span className="w-11/12">
-      <h3 className="text-white overflow-wrap break-words w-full">{task}</h3>
-      <h2 className="text-gray-400 w-full">{phrase}</h2>
+        <h3 className="text-white overflow-wrap break-words w-full">{task}</h3>
+        <h2 className="text-gray-400 w-full">{phrase}</h2>
       </span>
       
       <button
